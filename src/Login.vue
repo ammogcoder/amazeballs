23fd06c7836f0d3237b71fbe44da2c6cfc0b37fc
Lukas van Driel <lukas@q42.nl>
2018-01-08 22:57:23 +0100

    sign in with google / username&password

--- src/Login.vue
+++ src/Login.vue
@@ -1,5 +1,35 @@
 <template>
   <div>
-    login
+    <form @submit.prevent="signInWithEmailAndPassword">
+      <input v-model="email" placeholder="Email" />
+      <input v-model="password" placeholder="Password" />
+      <input type="submit" />
+    </form>
+
+    <button @click.prevent="signInWithGoogle">Log in with Google</button>
   </div>
 </template>
+
+<script>
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+export default {
+  name: 'inputform',
+  data() {
+    return {
+      email: null,
+      password: null,
+    };
+  },
+  methods: {
+    signInWithEmailAndPassword() {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password);
+    },
+    signInWithGoogle() {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider);
+    }
+  }
+}
+</script>
