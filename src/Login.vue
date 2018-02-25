c99a016d5b329eda1796a651efc70aecd3b2128d
Lukas van Driel <lukas@q42.nl>
2018-02-25 14:07:35 +0100

    removed username/password login it needs registration flow to work

--- src/Login.vue
+++ src/Login.vue
@@ -1,11 +1,5 @@
 <template>
   <div>
-    <form @submit.prevent="signInWithEmailAndPassword">
-      <input v-model="email" placeholder="Email" />
-      <input v-model="password" placeholder="Password" />
-      <input type="submit" />
-    </form>
-
     <button @click.prevent="signInWithGoogle">Log in with Google</button>
   </div>
 </template>
@@ -15,17 +9,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 export default {
-  data() {
-    return {
-      email: null,
-      password: null,
-    };
-  },
   methods: {
-    signInWithEmailAndPassword() {
-      firebase.auth().signInWithEmailAndPassword(this.email, this.password)
-        .catch((err) => alert(err.message || err));
-    },
     signInWithGoogle() {
       var provider = new firebase.auth.GoogleAuthProvider();
       firebase.auth().signInWithPopup(provider)
