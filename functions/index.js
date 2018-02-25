6b41407b63dc07d77d0a5aadf11c9c379b022ce7
Lukas van Driel <lukas@q42.nl>
2018-02-25 14:27:18 +0100

    cloud function that appends data to user's text

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.createBall = functions.firestore
+  .document('balls/{ballId}')
+  .onCreate(event => {
+    var createdMessage = event.data.get('message');
+
+    // this function will be run "at least once"
+    if (createdMessage.indexOf(', yo!') > 0) return;
+
+    return event.data.ref.set({
+      message: createdMessage + ', yo!'
+    }, {merge: true});
+});
