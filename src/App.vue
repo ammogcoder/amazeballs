8fc1db8d48945de4f13b380aafcf93f00c40667c
Lukas van Driel <lukas@q42.nl>
2018-03-01 22:33:37 +0100

    stylingz

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="logo">Amazeballs</div>
     <Login v-if="!store.currentUser" />
     <div v-else>
       <button @click="logout">Log out</button>
@@ -61,5 +61,26 @@ li {
 
 a {
   color: #42b983;
+}
+.logo {
+  font-size: 300%;
+  font-weight: bold;
+  font-variant: small-caps;
+  text-shadow: 2px 2px #ff0000;
+}
+
+input, button {
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-shadow: inset 0 1px 3px #ddd;
+  border-radius: 4px;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 12px;
+  padding-bottom: 12px;
 }
 </style>
